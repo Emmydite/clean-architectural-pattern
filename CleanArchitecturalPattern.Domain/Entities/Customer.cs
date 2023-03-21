c6b6a4d6a1aa8af6cac615ada2b9afceb998b51b
Emmanuel <emmacomessi@gmail.com>
2023-03-21 13:41:10 +0000

    made firstName property nullable

--- CleanArchitecturalPattern.Domain/Entities/Customer.cs
+++ CleanArchitecturalPattern.Domain/Entities/Customer.cs
@@ -9,7 +9,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
     public class Customer
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Postcode { get; set; }

385a6f7d4c4b15e78da8e8ead71312fc2d8b2e9b
Emmanuel <emmacomessi@gmail.com>
2023-03-22 17:07:04 +0000

    added product entity class fields

--- CleanArchitecturalPattern.Domain/Entities/Product.cs
+++ CleanArchitecturalPattern.Domain/Entities/Product.cs
@@ -8,6 +8,11 @@ namespace CleanArchitecturalPattern.Domain.Entities
 {
     public class Product
     {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductSKU { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
 
     }
 }

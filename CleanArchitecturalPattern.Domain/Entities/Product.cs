aa58c9eb7cffe4ba8f5895ed7ff79e2a3c85e081
Emmanuel <emmacomessi@gmail.com>
2023-03-22 17:07:42 +0000

    made product description nullable

--- CleanArchitecturalPattern.Domain/Entities/Product.cs
+++ CleanArchitecturalPattern.Domain/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public Guid Id { get; set; }
         public string ProductName { get; set; }
         public string ProductSKU { get; set; }
-        public string ProductDescription { get; set; }
+        public string? ProductDescription { get; set; }
         public decimal ProductPrice { get; set; }
 
     }

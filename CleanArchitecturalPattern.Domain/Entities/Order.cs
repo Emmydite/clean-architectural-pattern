5a02d867635fe5c70ec797bd53bc9d38c6c7667f
Emmanuel <emmacomessi@gmail.com>
2023-03-23 08:56:31 +0000

    added order entity fields

--- CleanArchitecturalPattern.Domain/Entities/Order.cs
+++ CleanArchitecturalPattern.Domain/Entities/Order.cs
@@ -8,5 +8,9 @@ namespace CleanArchitecturalPattern.Domain.Entities
 {
     public class Order
     {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid CustomerId { get; set; }
+        
     }
 }

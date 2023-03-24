191714ca9b621dc5318b68211d8e217399f3aef2
Emmanuel <emmacomessi@gmail.com>
2023-03-24 06:02:53 +0000

    added phone number field to customer entity

--- CleanArchitecturalPattern.Domain/Entities/Customer.cs
+++ CleanArchitecturalPattern.Domain/Entities/Customer.cs
@@ -13,5 +13,6 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Postcode { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }

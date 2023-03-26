5a10732bd8965984420b2659140248a5c2a2173b
Emmanuel <emmacomessi@gmail.com>
2023-03-26 13:33:10 +0100

    added city field to customer entity

--- CleanArchitecturalPattern.Domain/Entities/Customer.cs
+++ CleanArchitecturalPattern.Domain/Entities/Customer.cs
@@ -15,6 +15,6 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public string? Email { get; set; }
         public string Address { get; set; }
         public string Postcode { get; set; }
-        
+        public string City { get; set; }
     }
 }

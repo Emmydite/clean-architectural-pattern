6bb2cb9d4ade8c543a260ea1e0c10f13c485f5ee
Emmanuel <emmacomessi@gmail.com>
2023-03-28 16:03:06 +0100

    changed address to address1

--- CleanArchitecturalPattern.Domain/Entities/Customer.cs
+++ CleanArchitecturalPattern.Domain/Entities/Customer.cs
@@ -13,7 +13,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string? Email { get; set; }
-        public string Address { get; set; }
+        public string Address1 { get; set; }
         public string Postcode { get; set; }
         public string City { get; set; }
         public string Country { get; set; }

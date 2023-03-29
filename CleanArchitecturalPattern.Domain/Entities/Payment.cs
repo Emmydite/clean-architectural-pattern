de8e6536d89e13b29acf7a8f52f11e083290d62e
Emmanuel <emmacomessi@gmail.com>
2023-03-29 09:11:09 +0100

    added amount field

--- CleanArchitecturalPattern.Domain/Entities/Payment.cs
+++ CleanArchitecturalPattern.Domain/Entities/Payment.cs
@@ -11,5 +11,6 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public int Id { get; set; }
         public Guid CustomerId { get; set; }
         public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
     }
 }

2573dffb27de3dedb415cd03d69aa01a0be4eb8c
Emmanuel <emmacomessi@gmail.com>
2023-04-01 12:06:09 +0100

    converted order date to nullable

--- CleanArchitecturalPattern.Domain/Entities/Order.cs
+++ CleanArchitecturalPattern.Domain/Entities/Order.cs
@@ -11,7 +11,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public Guid OrderId { get; set; }
         public Guid ProductId { get; set; }
         public Guid CustomerId { get; set; }
-        public DateTime OrderDate { get; set; }
+        public DateTime? OrderDate { get; set; }
         public DateTime? OrderShippedDate { get; set; }
         public int Status { get;}
         public List<Order> Items { get; set; }

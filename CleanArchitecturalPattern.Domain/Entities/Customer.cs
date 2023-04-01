7ffc214ad93f9efaf5930508278814b3dc104fdc
Emmanuel <emmacomessi@gmail.com>
2023-04-01 12:04:13 +0100

    made city nullable

--- CleanArchitecturalPattern.Domain/Entities/Customer.cs
+++ CleanArchitecturalPattern.Domain/Entities/Customer.cs
@@ -16,7 +16,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
         public string Address1 { get; set; }
         public string? Address2 { get; set; }
         public string Postcode { get; set; }
-        public string City { get; set; }
+        public string? City { get; set; }
         public string Country { get; set; }
     }
 }

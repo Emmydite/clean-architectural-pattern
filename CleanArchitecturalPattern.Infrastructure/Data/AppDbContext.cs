91bda86310970ffd724073145fccb8010e8f8f43
Emmanuel <emmacomessi@gmail.com>
2023-04-04 12:42:17 +0100

    add Order entity to dbset

--- CleanArchitecturalPattern.Infrastructure/Data/AppDbContext.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/AppDbContext.cs
@@ -15,5 +15,6 @@ namespace CleanArchitecturalPattern.Infrastructure.Data
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product>  Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }

8179c05d815d09a99f4a3d8cae2e168ed6bca167
Emmanuel <emmacomessi@gmail.com>
2023-04-04 12:41:32 +0100

    Add Product to dbset

--- CleanArchitecturalPattern.Infrastructure/Data/AppDbContext.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace CleanArchitecturalPattern.Infrastructure.Data
                            : base(options) { }
 
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product>  Products { get; set; }
     }
 }

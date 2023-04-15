35384d6fac07be2c9a1437d25650df451650bda6
Emmanuel <emmacomessi@gmail.com>
2023-04-15 13:40:37 +0100

    reference to payment entity

--- CleanArchitecturalPattern.Application/Interfaces/Repositories/IPaymentRepository.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Repositories/IPaymentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Application.Interfaces.Repositories
 {
-    public interface IPaymentRepository : ISharedRepository
+    public interface IPaymentRepository : ISharedRepository<Payment>
     {
     }
 }

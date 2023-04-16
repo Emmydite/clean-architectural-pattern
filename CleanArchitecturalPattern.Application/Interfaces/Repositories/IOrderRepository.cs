ab3289a0a0252b24701ebc9f6b32761063a1b24a
Emmanuel <emmacomessi@gmail.com>
2023-04-16 09:37:46 +0100

    added get customer orders interface endpoint

--- CleanArchitecturalPattern.Application/Interfaces/Repositories/IOrderRepository.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Repositories
 {
     public interface IOrderRepository
     {
+        Task<IEnumerable<Order>> GetCustomerOrdersById(Guid customerId);
     }
 }

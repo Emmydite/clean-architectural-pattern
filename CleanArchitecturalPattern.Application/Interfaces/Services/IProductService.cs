7dc591bb1a24c222b69c5960a0e6360c4ad98750
Emmanuel <emmacomessi@gmail.com>
2023-04-19 13:02:10 +0100

    converted to public access modifier

--- CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Application.Interfaces.Services
 {
-    internal interface IProductService
+    public interface IProductService
     {
     }
 }

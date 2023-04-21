b52bc544b56357ce955a71f01c0e883bc0268ddf
Emmanuel <emmacomessi@gmail.com>
2023-04-21 15:34:55 +0100

    added customer entity reference

--- CleanArchitecturalPattern.Application/Interfaces/Services/ICustomerService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/ICustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
 {
     public interface ICustomerService
     {
-
+        Customer
     }
 }

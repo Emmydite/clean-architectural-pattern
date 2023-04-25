c88e98a76e13c4de68353c4e5b31ef4c61d55983
Emmanuel <emmacomessi@gmail.com>
2023-04-25 06:53:30 +0100

    added Add product interface endpoint

--- CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
 {
     public interface IProductService
     {
+        Product AddProduct(Product product);
     }
 }

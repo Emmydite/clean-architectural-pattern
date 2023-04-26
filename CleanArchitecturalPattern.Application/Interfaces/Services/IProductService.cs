c50e63f0d53229f99c6d7511df81e413f9ce26fb
Emmanuel <emmacomessi@gmail.com>
2023-04-26 09:32:48 +0100

    parameter to product id

--- CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
@@ -11,6 +11,6 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
     {
         Task<Product> AddProduct(Product product);
         Task<bool> UpdateProduct(Product product);
-        Task<bool> DeleteProduct(Product product);
+        Task<bool> DeleteProduct(int id);
     }
 }

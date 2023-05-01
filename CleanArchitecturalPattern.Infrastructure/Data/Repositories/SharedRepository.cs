171a4472a39b2cc602164556fdf709eee1b8cab8
Emmanuel <emmacomessi@gmail.com>
2023-05-01 09:06:04 +0100

    access modifier to public

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
-    internal class SharedRepository
+    public class SharedRepository
     {
     }
 }

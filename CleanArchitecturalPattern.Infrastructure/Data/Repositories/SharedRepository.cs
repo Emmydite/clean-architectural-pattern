4e9192489b64f7a648b09ba46c5ccf8c78e80fd8
Emmanuel <emmacomessi@gmail.com>
2023-05-01 09:09:47 +0100

    convert to generic class

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Application.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
-    public class SharedRepository<TEntity>
+    public class SharedRepository<TEntity> : ISharedRepository<TEntity> where TEntity : class
     {
 
     }

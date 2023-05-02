e12ced9896bd7d3edd965e20b24613cfd1106305
Emmanuel <emmacomessi@gmail.com>
2023-05-02 07:16:54 +0100

    implement IShared repo interfaces

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/SharedRepository.cs
@@ -9,6 +9,34 @@ namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
     public class SharedRepository<TEntity> : ISharedRepository<TEntity> where TEntity : class
     {
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

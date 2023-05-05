5d09a8e4a7e6199927203fd1ece0a78ca891cc94
Emmanuel <emmacomessi@gmail.com>
2023-05-05 13:19:40 +0100

    convert return type to boolean

--- CleanArchitecturalPattern.Application/Interfaces/Repositories/ISharedRepository.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Repositories/ISharedRepository.cs
@@ -9,7 +9,7 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Repositories
     public interface ISharedRepository<T> where T : class
     {
         Task AddAsync(T entity);
-        Task<bool> Update(T entity);
+        bool Update(T entity);
         void Delete(T entity);
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(Guid id);

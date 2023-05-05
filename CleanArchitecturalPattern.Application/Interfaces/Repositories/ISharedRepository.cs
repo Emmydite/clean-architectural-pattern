a097f1bf632cf2e1867c5f9d54955063a90a25ca
Emmanuel <emmacomessi@gmail.com>
2023-05-05 13:29:41 +0100

    change interface endpoint return type to void

--- CleanArchitecturalPattern.Application/Interfaces/Repositories/ISharedRepository.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Repositories/ISharedRepository.cs
@@ -9,7 +9,7 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Repositories
     public interface ISharedRepository<T> where T : class
     {
         Task AddAsync(T entity);
-        bool Update(T entity);
+        void Update(T entity);
         void Delete(T entity);
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(Guid id);

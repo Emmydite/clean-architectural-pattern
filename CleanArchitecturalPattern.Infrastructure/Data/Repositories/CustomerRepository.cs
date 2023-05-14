70c45473cd404f260c50e4f496638466c3f5b896
Emmanuel <emmacomessi@gmail.com>
2023-05-14 17:01:19 +0100

    code refactor

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/CustomerRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/CustomerRepository.cs
@@ -17,7 +17,9 @@ namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 
         public async Task<IEnumerable<Customer>> GetCustomerByEmail(string customerEmail)
         {
-            return await _appDbContext.Customers.Where(e => e.Email == customerEmail).FirstOrDefault();
+            return await _appDbContext.Customers
+                                      .Where(e => e.Email == customerEmail)
+                                      .FirstOrDefault();
         }
     }
 }

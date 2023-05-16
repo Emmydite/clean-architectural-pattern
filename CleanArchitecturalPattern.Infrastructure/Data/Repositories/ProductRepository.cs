4978275d61d30488480ece9cfdae6e7c8e519415
Emmanuel <emmacomessi@gmail.com>
2023-05-16 09:12:41 +0100

    inherit product repository interface

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/ProductRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/ProductRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
-    public class ProductRepository : SharedRepository<Product>
+    public class ProductRepository : SharedRepository<Product>, IProductRepository
     {
     }
 }

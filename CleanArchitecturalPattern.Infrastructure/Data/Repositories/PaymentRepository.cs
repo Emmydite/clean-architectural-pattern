9193fb6f1a8804f54fb5a8792917fc5bdd0f3757
Emmanuel <emmacomessi@gmail.com>
2023-05-16 09:19:12 +0100

    convert access modifier to public

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/PaymentRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/PaymentRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
-    internal class PaymentRepository
+    public class PaymentRepository
     {
     }
 }

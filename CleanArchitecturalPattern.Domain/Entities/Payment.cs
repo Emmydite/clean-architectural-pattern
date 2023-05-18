e982281c45f567fb12d1fd7d4930af890b9b5db7
Emmanuel <emmacomessi@gmail.com>
2023-05-18 13:37:30 +0100

    add orderId field to payment entity

--- CleanArchitecturalPattern.Domain/Entities/Payment.cs
+++ CleanArchitecturalPattern.Domain/Entities/Payment.cs
@@ -10,6 +10,7 @@ namespace CleanArchitecturalPattern.Domain.Entities
     {
         public int Id { get; set; }
         public Guid CustomerId { get; set; }
+        public Guid OrderId { get; set; }
         public DateTime PaymentDate { get; set; }
         public decimal Amount { get; set; }
     }

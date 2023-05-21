0b32bc2c3326fab311d4df7c0a9ac91500dfab7b
Emmanuel <emmacomessi@gmail.com>
2023-05-21 17:02:25 +0100

    convert to public modifier

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/OrderRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/OrderRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
-    internal class OrderRepository
+    public class OrderRepository
     {
     }
 }

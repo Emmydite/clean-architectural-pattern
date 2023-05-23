33326af38027282f282e33d5e0736aa7f23f1bb1
Emmanuel <emmacomessi@gmail.com>
2023-05-23 10:04:50 +0100

    await keyword for astnchronous operation

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/OrderRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/OrderRepository.cs
@@ -17,7 +17,7 @@ namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 
         public async Task<IEnumerable<Order>> GetCustomerOrdersById(Guid customerId)
         {
-            var result = _appDbContext.Orders.Where(e => e.CustomerId == customerId).ToList();
+            var result = await _appDbContext.Orders.Where(e => e.CustomerId == customerId).ToList();
 
             return result;
         }

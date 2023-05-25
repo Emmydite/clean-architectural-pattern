10d96b4b65f54436c565b117917f3a8d9986ba23
Emmanuel <emmacomessi@gmail.com>
2023-05-25 08:09:20 +0100

    add constructor

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/LineItemRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/LineItemRepository.cs
@@ -8,5 +8,9 @@ namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories
 {
     public class LineItemRepository
     {
+        public LineItemRepository()
+        {
+
+        }
     }
 }

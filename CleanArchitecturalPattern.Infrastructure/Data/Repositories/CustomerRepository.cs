44d01c566b42506359cb179e3b7383ffbc687ca8
Emmanuel <emmacomessi@gmail.com>
2023-05-29 09:07:45 +0100

    removed used directives

--- CleanArchitecturalPattern.Infrastructure/Data/Repositories/CustomerRepository.cs
+++ CleanArchitecturalPattern.Infrastructure/Data/Repositories/CustomerRepository.cs
@@ -2,9 +2,6 @@
 using CleanArchitecturalPattern.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.Data.Repositories

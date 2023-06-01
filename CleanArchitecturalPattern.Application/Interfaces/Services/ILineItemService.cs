56e2e4625fdd4f5f8e25998d18af7d048493d2f3
Emmanuel <emmacomessi@gmail.com>
2023-06-01 13:56:01 +0100

    added delete lineitem interface endpoint

--- CleanArchitecturalPattern.Application/Interfaces/Services/ILineItemService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/ILineItemService.cs
@@ -6,5 +6,6 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
     public interface ILineItemService
     {
         Task<LineItem> AddLineItem(LineItem lineItem);
+        void DeleteLineItem(Guid id);
     }
 }

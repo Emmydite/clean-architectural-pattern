27a5954baf70feb069ba4ae1f81de708e52ff948
Emmanuel <emmacomessi@gmail.com>
2023-06-05 13:31:54 +0100

    missing semi colon

--- CleanArchitecturalPattern.Application/Services/CustomerService.cs
+++ CleanArchitecturalPattern.Application/Services/CustomerService.cs
@@ -1,5 +1,5 @@
 ﻿using CleanArchitecturalPattern.Application.Interfaces.Services;
-using CleanArchitecturalPattern.Application.Interfaces.Repositories
+using CleanArchitecturalPattern.Application.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;

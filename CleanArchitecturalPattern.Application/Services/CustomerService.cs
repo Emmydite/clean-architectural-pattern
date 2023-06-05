f60b006b86cc6549e5c19df7833a37f2ef44f22f
Emmanuel <emmacomessi@gmail.com>
2023-06-05 13:31:32 +0100

    missing semi colon

--- CleanArchitecturalPattern.Application/Services/CustomerService.cs
+++ CleanArchitecturalPattern.Application/Services/CustomerService.cs
@@ -10,7 +10,7 @@ namespace CleanArchitecturalPattern.Application.Services
 {
     public class CustomerService : ICustomerService
     {
-        private readonly ICustomerRepository _customerRepository
+        private readonly ICustomerRepository _customerRepository;
         public CustomerService(ICustomerRepository customerRepository)
         {
 

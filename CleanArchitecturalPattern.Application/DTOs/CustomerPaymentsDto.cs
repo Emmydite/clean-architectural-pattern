462b6dec8803d24c5cacb8a0414e6e1f8ef6655e
Emmanuel <emmacomessi@gmail.com>
2023-06-19 13:08:04 +0100

    convert to public access modifier

--- CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
+++ CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Application.DTOs
 {
-    internal class CustomerPaymentsDto
+    public class CustomerPaymentsDto
     {
     }
 }

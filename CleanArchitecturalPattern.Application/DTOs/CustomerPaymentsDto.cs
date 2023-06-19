1833b55790cb96c1cea0c97c4cf882f9ca89dc06
Emmanuel <emmacomessi@gmail.com>
2023-06-19 13:11:55 +0100

    customer phone field

--- CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
+++ CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
@@ -11,5 +11,7 @@ namespace CleanArchitecturalPattern.Application.DTOs
         public Guid CustomerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        
     }
 }

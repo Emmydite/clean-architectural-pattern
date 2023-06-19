e93450931a228d543d53415c6fcdb47407a1c312
Emmanuel <emmacomessi@gmail.com>
2023-06-19 13:12:11 +0100

    customer email field

--- CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
+++ CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
@@ -12,6 +12,6 @@ namespace CleanArchitecturalPattern.Application.DTOs
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        
+        public string? Email { get; set; }
     }
 }

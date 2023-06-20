aa00ba0c3565ce3babfc601b0219908448af6178
Emmanuel <emmacomessi@gmail.com>
2023-06-20 18:05:33 +0100

    nullify last name

--- CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
+++ CleanArchitecturalPattern.Application/DTOs/CustomerPaymentsDto.cs
@@ -10,7 +10,7 @@ namespace CleanArchitecturalPattern.Application.DTOs
     {
         public Guid CustomerId { get; set; }
         public string? FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
         public Guid OrderId { get; set; }

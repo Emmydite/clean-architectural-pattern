036bbd00934370d8359e024ae55c47f4348fe73a
Emmanuel <emmacomessi@gmail.com>
2023-06-21 08:46:44 +0100

    reference services interface

--- CleanArchitecturalPattern.Application/Services/ProductService.cs
+++ CleanArchitecturalPattern.Application/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Application.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Application.Services
 {
-    public class ProductService
+    public class ProductService 
     {
         public ProductService()
         {

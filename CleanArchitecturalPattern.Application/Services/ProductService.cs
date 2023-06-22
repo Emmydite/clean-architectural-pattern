68a8ff6897beb1843cde362c040d313f3d31641a
Emmanuel <emmacomessi@gmail.com>
2023-06-22 09:00:59 +0100

    throw exception

--- CleanArchitecturalPattern.Application/Services/ProductService.cs
+++ CleanArchitecturalPattern.Application/Services/ProductService.cs
@@ -25,7 +25,7 @@ namespace CleanArchitecturalPattern.Application.Services
             }
             catch (Exception ex)
             {
-                
+                throw ex;
             }
         }
     }

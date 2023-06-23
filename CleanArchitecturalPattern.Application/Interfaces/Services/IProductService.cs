ef0fdbd87546b0b550b7b1b26610d727bcf540ee
Emmanuel <emmacomessi@gmail.com>
2023-06-23 14:07:03 +0100

    change interface return type to integer

--- CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/IProductService.cs
@@ -4,7 +4,7 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
 {
     public interface IProductService
     {
-        Task<Product> AddProduct(Product product);
+        Task<int> AddProduct(Product product);
         Task<bool> UpdateProduct(Product product);
         void DeleteProduct(Guid id);
         Task<Product> GetProductById(Guid id);

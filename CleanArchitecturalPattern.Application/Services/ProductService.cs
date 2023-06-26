bc8be9a0281679385e749e904073ce6e669b0bf8
Emmanuel <emmacomessi@gmail.com>
2023-06-26 08:58:26 +0100

    missing semi colon

--- CleanArchitecturalPattern.Application/Services/ProductService.cs
+++ CleanArchitecturalPattern.Application/Services/ProductService.cs
@@ -43,7 +43,7 @@ namespace CleanArchitecturalPattern.Application.Services
             }
             catch (Exception ex)
             {
-                throw ex
+                throw ex;
             }
             
         }

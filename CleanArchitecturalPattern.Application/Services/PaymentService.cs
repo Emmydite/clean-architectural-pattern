3d5c66c1bdd94da637f197a637b58c41daab4433
Emmanuel <emmacomessi@gmail.com>
2023-07-14 08:51:29 +0100

    throw exception

--- CleanArchitecturalPattern.Application/Services/PaymentService.cs
+++ CleanArchitecturalPattern.Application/Services/PaymentService.cs
@@ -103,7 +103,7 @@ namespace CleanArchitecturalPattern.Application.Services
             }
             catch (Exception ex)
             {
-
+                throw ex;
             }
         }
     }

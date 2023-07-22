78dcff08e4333f76300678181b2d74ffa429a5eb
Emmanuel <emmacomessi@gmail.com>
2023-07-22 13:01:47 +0100

    call to add line item

--- CleanArchitecturalPattern.Application/Services/LineItemService.cs
+++ CleanArchitecturalPattern.Application/Services/LineItemService.cs
@@ -21,7 +21,7 @@ namespace CleanArchitecturalPattern.Application.Services
         {
             try
             {
-
+                _lineItemRepository.AddAsync()
             }
             catch (Exception ex) 
             {

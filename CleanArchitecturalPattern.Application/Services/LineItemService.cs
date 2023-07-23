6675a85cecb989b2aeda0150f87f0f6789e8ada4
Emmanuel <emmacomessi@gmail.com>
2023-07-23 09:02:37 +0100

    get result

--- CleanArchitecturalPattern.Application/Services/LineItemService.cs
+++ CleanArchitecturalPattern.Application/Services/LineItemService.cs
@@ -35,7 +35,7 @@ namespace CleanArchitecturalPattern.Application.Services
         {
             try
             {
-                var lineItem = _lineItemRepository.GetByIdAsync(id).GetAwaiter()
+                var lineItem = _lineItemRepository.GetByIdAsync(id).GetAwaiter().GetResult();
             }
             catch ()
             { 

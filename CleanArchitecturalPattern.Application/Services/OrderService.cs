df1c593579a3b3ee5c5847bfaafc943b56f69804
Emmanuel <emmacomessi@gmail.com>
2023-08-02 08:03:02 +0100

    reference to repository intreface folder

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Application.Interfaces.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace CleanArchitecturalPattern.Application.Services
 {
     public class OrderService
     {
+        
         public OrderService()
         {
 

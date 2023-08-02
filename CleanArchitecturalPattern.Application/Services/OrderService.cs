d5503763b20ce6c5d3f2223476caaa3a0cfa6991
Emmanuel <emmacomessi@gmail.com>
2023-08-02 08:05:23 +0100

    order service interface folder

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchitecturalPattern.Application.Interfaces.Repositories;
+using CleanArchitecturalPattern.Application.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;

d2653ac7cc0db7cb520d5494adc58ab3bf06d98d
Emmanuel <emmacomessi@gmail.com>
2023-08-04 16:35:12 +0100

    entities folder directive

--- CleanArchitecturalPattern.Application/Interfaces/Services/IOrderService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/IOrderService.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using CleanArchitecturalPattern.Domain.Entities;
+using System;
 
 
 namespace CleanArchitecturalPattern.Application.Interfaces.Services
 {
     public interface IOrderService
     {
+        Task<int> AddLineItem();
     }
 }

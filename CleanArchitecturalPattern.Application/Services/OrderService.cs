1ddd82ef24cbdad3377b35cc71295750c9ef1195
Emmanuel <emmacomessi@gmail.com>
2023-08-06 08:26:43 +0100

    entity directive

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchitecturalPattern.Application.Interfaces.Repositories;
 using CleanArchitecturalPattern.Application.Interfaces.Services;
+using CleanArchitecturalPattern.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,9 @@ namespace CleanArchitecturalPattern.Application.Services
             _orderRepository = orderRepository;
         }
 
+        public Task<int> AddOrder()
+        {
 
+        }
     }
 }

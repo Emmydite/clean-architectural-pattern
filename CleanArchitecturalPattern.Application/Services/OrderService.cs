37585517faab3b184524cf5f2b7a5964517b0abe
Emmanuel <emmacomessi@gmail.com>
2023-08-07 13:22:06 +0100

    convert to async

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -15,7 +15,7 @@ namespace CleanArchitecturalPattern.Application.Services
             _orderRepository = orderRepository;
         }
 
-        public Task<int> AddOrder(Order order)
+        public async Task<int> AddOrder(Order order)
         {
             try
             {

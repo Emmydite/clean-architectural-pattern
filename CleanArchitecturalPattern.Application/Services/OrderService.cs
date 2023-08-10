2e2f18b6b6aed1de48740b43f8b96cff83607146
Emmanuel <emmacomessi@gmail.com>
2023-08-10 06:12:06 +0100

    get result

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -34,7 +34,7 @@ namespace CleanArchitecturalPattern.Application.Services
         {
             try
             {
-                var order = _orderRepository.GetByIdAsync(id).GetAwaiter()
+                var order = _orderRepository.GetByIdAsync(id).GetAwaiter().GetResult();
             }
             catch (Exception ex) 
             {

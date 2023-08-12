411d440ad064ec90f6dc82a03d7fb7a818b91931
Emmanuel <emmacomessi@gmail.com>
2023-08-12 08:53:13 +0100

    repository update order

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -54,7 +54,7 @@ namespace CleanArchitecturalPattern.Application.Services
         {
             try
             {
-
+                _orderRepository.Update();
             }
             catch (Exception ex)
             {

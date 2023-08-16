48556e0ac04c98c3c87848e397275148c71e11eb
Emmanuel <emmacomessi@gmail.com>
2023-08-16 09:47:46 +0100

    return order result

--- CleanArchitecturalPattern.Application/Services/OrderService.cs
+++ CleanArchitecturalPattern.Application/Services/OrderService.cs
@@ -70,6 +70,7 @@ namespace CleanArchitecturalPattern.Application.Services
             try
             {
                 var order = _orderRepository.GetByIdAsync(id);
+                return order;
             }
             catch (Exception ex) 
             {

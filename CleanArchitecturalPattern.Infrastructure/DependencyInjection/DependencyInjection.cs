3277546d31b5baed1e9a2abf27292cfde59d13ed
Emmanuel <emmacomessi@gmail.com>
2023-08-21 08:55:29 +0100

    convert class to public access modifier

--- CleanArchitecturalPattern.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ CleanArchitecturalPattern.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Infrastructure.DependencyInjection
 {
-    internal class DependencyInjection
+    public class DependencyInjection
     {
     }
 }

5e949277d896afbec9a27d994222ea07d6d2414c
Emmanuel <emmacomessi@gmail.com>
2023-08-29 09:19:08 +0100

    code clean up

--- CleanArchitecturalPattern.Application/DTOs/Product.cs
+++ CleanArchitecturalPattern.Application/DTOs/Product.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchitecturalPattern.Application.DTOs
 {
     public class Product
     {
+
     }
 }

36ec4efe0ff38ce154d687c2f3617c4e0ca9e281
Emmanuel <emmacomessi@gmail.com>
2023-09-11 13:08:55 +0100

    convert to async

--- CleanArchitecturalPattern.WebApi/Controllers/ProductController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/ProductController.cs
@@ -72,7 +72,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
             }
         }
 
-        public Task<IActionResult> UpdateProduct()
+        public async Task<IActionResult> UpdateProduct()
         {
             try
             {

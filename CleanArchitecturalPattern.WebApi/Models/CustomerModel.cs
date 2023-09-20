9cb3c981e8e21ce1da2bf82e088bd2a932888f34
Emmanuel <emmacomessi@gmail.com>
2023-09-20 10:04:59 +0100

    model phone field

--- CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
@@ -7,5 +7,6 @@ namespace CleanArchitecturalPattern.WebApi.Models
         public Guid Id { get; set; }
         public string? FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }

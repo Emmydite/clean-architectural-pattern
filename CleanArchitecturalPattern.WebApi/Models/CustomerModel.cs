3a5b9e0dcbc0477df960f065cbb68066a97692be
Emmanuel <emmacomessi@gmail.com>
2023-09-20 10:05:31 +0100

    model email field

--- CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
@@ -8,5 +8,6 @@ namespace CleanArchitecturalPattern.WebApi.Models
         public string? FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
     }
 }

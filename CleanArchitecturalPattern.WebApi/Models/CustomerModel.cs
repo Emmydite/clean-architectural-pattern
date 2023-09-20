847e88220e35d9222aadfcfbfaf6076705ee0345
Emmanuel <emmacomessi@gmail.com>
2023-09-20 10:07:35 +0100

    model city field

--- CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
@@ -12,5 +12,6 @@ namespace CleanArchitecturalPattern.WebApi.Models
         public string Address1 { get; set; }
         public string? Address2 { get; set; }
         public string Postcode { get; set; }
+        public string? City { get; set; }
     }
 }

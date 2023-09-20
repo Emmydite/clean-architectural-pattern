4c2ae7971ca2d85030afaf10691c50ffe0ad4eda
Emmanuel <emmacomessi@gmail.com>
2023-09-20 10:06:33 +0100

    model address2 field

--- CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/CustomerModel.cs
@@ -10,5 +10,6 @@ namespace CleanArchitecturalPattern.WebApi.Models
         public string PhoneNumber { get; set; }
         public string? Email { get; set; }
         public string Address1 { get; set; }
+        public string? Address2 { get; set; }
     }
 }

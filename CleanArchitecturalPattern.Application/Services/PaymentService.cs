26b328eebacdff68391058bd3dcdbec4300c30bc
Emmanuel <emmacomessi@gmail.com>
2023-09-24 17:15:25 +0100

    removed unused directives

--- CleanArchitecturalPattern.Application/Services/PaymentService.cs
+++ CleanArchitecturalPattern.Application/Services/PaymentService.cs
@@ -2,9 +2,6 @@
 using CleanArchitecturalPattern.Application.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CleanArchitecturalPattern.Domain.Entities;
 
 namespace CleanArchitecturalPattern.Application.Services

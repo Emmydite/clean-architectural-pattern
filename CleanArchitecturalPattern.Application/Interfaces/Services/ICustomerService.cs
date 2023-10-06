60a87b2553c9b938bb8f6f100b6c91f07ed3f9d0
Emmanuel <emmacomessi@gmail.com>
2023-10-06 09:47:38 +0100

    change return type to boolean

--- CleanArchitecturalPattern.Application/Interfaces/Services/ICustomerService.cs
+++ CleanArchitecturalPattern.Application/Interfaces/Services/ICustomerService.cs
@@ -7,7 +7,7 @@ namespace CleanArchitecturalPattern.Application.Interfaces.Services
     {
         Task<int> AddCustomer(Customer customer);
         void DeleteCustomer(Guid id);
-        void UpdateCustomer(Customer customer);
+        bool UpdateCustomer(Customer customer);
         Task<IEnumerable<Customer>> GetAllCustomers();
         Task<Customer> GetCustomer(Guid id);
         Task<List<CustomerPaymentsDto>> CustomerPayments(Guid id);

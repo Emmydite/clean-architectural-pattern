b4b05d3d7c91f753529d9afd6c26bb98987b0e93
Emmanuel <emmacomessi@gmail.com>
2023-10-13 07:06:18 +0100

    missing semi colon

--- CleanArchitecturalPattern.WebApi/Controllers/CustomerController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/CustomerController.cs
@@ -123,7 +123,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
         {
             try
             {
-                return Ok()
+                return Ok();
             }
             catch (Exception ex) 
             {

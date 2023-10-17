34e499b003714be00f109cbf5fb4f31f8b454917
Emmanuel <emmacomessi@gmail.com>
2023-10-17 08:01:26 +0100

    throw exception

--- CleanArchitecturalPattern.WebApi/Controllers/OrderController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/OrderController.cs
@@ -22,7 +22,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
             }
             catch (Exception ex) 
             { 
-
+                throw ex
             } 
         }
     }

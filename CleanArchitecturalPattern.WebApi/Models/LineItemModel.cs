23f8aa20a7062f0e1ec3b88517ed891edcb0183c
Emmanuel <emmacomessi@gmail.com>
2023-10-19 08:08:46 +0100

    get and set total price

--- CleanArchitecturalPattern.WebApi/Models/LineItemModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/LineItemModel.cs
@@ -8,9 +8,6 @@ namespace CleanArchitecturalPattern.WebApi.Models
         public Guid OrderId { get; set; }
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
-        public decimal TotalPrice()
-        {
-            return product.Price * Quantity;
-        }
+        public decimal TotalPrice { get; set; }
     }
 }

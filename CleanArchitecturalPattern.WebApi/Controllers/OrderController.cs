edabd8660be805258d13a9ec987f981c3e002911
Emmanuel <emmacomessi@gmail.com>
2023-11-03 17:23:14 +0000

    semi colon

--- CleanArchitecturalPattern.WebApi/Controllers/OrderController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/OrderController.cs
@@ -90,7 +90,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
         {
             try
             {
-                bool success = false
+                bool success = false;
                 var order = await _orderService.GetOrderById(model.OrderId);
                 if (order != null)
                 {

a268cba04024e9c7403210dd9d717dd558c55c48
Emmanuel <emmacomessi@gmail.com>
2023-11-07 08:56:36 +0000

    amount field

--- CleanArchitecturalPattern.WebApi/Models/PaymentModel.cs
+++ CleanArchitecturalPattern.WebApi/Models/PaymentModel.cs
@@ -5,5 +5,6 @@
         public Guid CustomerId { get; set; }
         public Guid OrderId { get; set; }
         public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
     }
 }

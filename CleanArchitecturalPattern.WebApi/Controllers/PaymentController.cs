e4f3ae81216e71d00dbc7ff625301aac4c868b38
Emmanuel <emmacomessi@gmail.com>
2023-11-14 07:03:52 +0000

    get payments endpoint

--- CleanArchitecturalPattern.WebApi/Controllers/PaymentController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/PaymentController.cs
@@ -51,5 +51,10 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
                 throw ex;
             }
         }
+
+        public IActionResult GetPayments() 
+        { 
+
+        }
     }
 }

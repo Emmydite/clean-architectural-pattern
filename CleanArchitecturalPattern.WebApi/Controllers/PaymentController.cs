128536d238bb2b637b975eba405b0722eb5c6557
Emmanuel <emmacomessi@gmail.com>
2023-11-17 17:50:11 +0000

    delete payment endpoint

--- CleanArchitecturalPattern.WebApi/Controllers/PaymentController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/PaymentController.cs
@@ -65,5 +65,10 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
                 throw ex;
             }
         }
+
+        public IActionResult DeletePayment()
+        {
+
+        }
     }
 }

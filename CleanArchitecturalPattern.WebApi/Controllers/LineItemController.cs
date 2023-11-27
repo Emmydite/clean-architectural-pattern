5462c7787240eef66f5289ca9bd6947daca8167d
Emmanuel <emmacomessi@gmail.com>
2023-11-27 08:10:08 +0000

    reference to interface services

--- CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CleanArchitecturalPattern.Application.Interfaces.Services;
 
 namespace CleanArchitecturalPattern.WebApi.Controllers
 {

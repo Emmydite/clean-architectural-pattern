72e13958f31e679c073b03eb8a74e99f52d93285
Emmanuel <emmacomessi@gmail.com>
2023-11-28 13:17:50 +0000

    throw exception

--- CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
@@ -23,7 +23,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
             }
             catch (Exception ex) 
             {
-            
+            throw ex
             } 
         }
     }

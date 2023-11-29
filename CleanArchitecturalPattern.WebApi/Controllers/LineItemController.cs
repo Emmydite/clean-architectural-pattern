463f2dd6b4e54c356c657319329c51cc8effb1f4
Emmanuel <emmacomessi@gmail.com>
2023-11-29 07:42:48 +0000

    map order id field

--- CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
@@ -22,7 +22,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
             {
                 var lineItem = new LineItem
                 {
-
+                    OrderId = model.OrderId,
                 }
             }
             catch (Exception ex) 

0b0806dc19633fd01578520b5d11f3f5c8d25990
Emmanuel <emmacomessi@gmail.com>
2023-12-05 18:48:49 +0000

    return ok status

--- CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
@@ -54,7 +54,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
         {
             try
             {
-
+                return Ok()
             }
             catch (Exception ex) 
             {

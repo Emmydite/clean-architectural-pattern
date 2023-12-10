42529f4442dfa348e93a6e5078121a4481f4b3e3
Emmanuel <emmacomessi@gmail.com>
2023-12-10 17:09:26 +0000

    model parameter

--- CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
+++ CleanArchitecturalPattern.WebApi/Controllers/LineItemController.cs
@@ -76,7 +76,7 @@ namespace CleanArchitecturalPattern.WebApi.Controllers
             }
         }
 
-        public async Task<IActionResult> UpdateLineItem()
+        public async Task<IActionResult> UpdateLineItem(LineItemModel model)
         {
             try
             {
